d2e7c53f9d7b9f28ed41962bc5a8fa51d383ed7c
BILL <valygnos@gmail.com>
2021-05-31 18:49:24 +0300

    Updates

---
+++ precache-manifest.e61b29a469e35660660628c92f9cd508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355b8707567f54e9e80651d8c320b93d",
+    "url": "/contract-app/index.html"
+  },
+  {
+    "revision": "4dae8d83b0d5905db6b9",
+    "url": "/contract-app/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "971fe42271fc796c05a3",
+    "url": "/contract-app/static/js/2.2bcc8803.chunk.js"
+  },
+  {
+    "revision": "4dae8d83b0d5905db6b9",
+    "url": "/contract-app/static/js/main.1c6a68ca.chunk.js"
+  },
+  {
+    "revision": "e7e3cbbff1f2699c504e",
+    "url": "/contract-app/static/js/runtime-main.46dbd41b.js"
+  }
+]);
